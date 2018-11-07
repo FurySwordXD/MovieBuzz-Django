8ec162ff54ef2021ba71dbed74f06811c0e1a893
FurySwordXD <sainathganesh1@gmail.com>
2018-11-07 19:05:09 +0530

    added theatre view

--- movies/urls.py
+++ movies/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('movie/<movie_name>/', views.movie),
+    path('theatre/<theatre_id>/', views.theatre),
     path('show/', views.show),
     path('book/', views.book),
     path('booking_info/', views.booking_info),
--- movies/views.py
+++ movies/views.py
@@ -3,13 +3,84 @@ import json
 # Create your views here.
 from django.shortcuts import render, HttpResponse, redirect
 from django.http import JsonResponse
-from .models import Show, Movie, Booking, Seat
+from .models import Show, Movie, Booking, Seat, Theatre
 
 def index(request):
     movies = set()
+    theatres = set()
     for show in Show.objects.all():
         movies.add(show.movie)
-    return render(request, 'index.html', {"user": request.user, "movies": movies})
+    for theatre in Theatre.objects.all():
+        theatres.add(theatre)
+    return render(request, 'index.html', {"user": request.user, "movies": movies, "theatres": theatres})
+
+def theatre(request, theatre_id):
+    theatre = Theatre.objects.get(id=theatre_id)
+    shows = Show.objects.filter(theatre=theatre)
+    data = []
+    for show in shows:
+        flag = False
+        for block in data:
+            if block['movie'] == show.movie.name:
+                date_flag = False
+                for per_day in block['per_day']:
+                    if per_day['date'] == show.get_date():
+                        screen_flag = False
+                        for screen in per_day['screens']:
+                            if screen['screen_name'] == show.screen.name:
+                                screen['shows'].append({"show_id": show.id, "time": show.get_time()})
+                                screen_flag = True
+                        if not screen_flag:
+                            per_day['screens'].append({"screen_name": show.screen.name, "shows": [{"show_id": show.id, "time": show.get_time()}]})
+                        date_flag = True
+                        flag = True
+                if not date_flag:
+                    block['per_day'].append({
+                        "date": show.get_date(), 
+                        "day": show.get_day(), 
+                        "screens": [
+                            {   "screen_name": show.screen.name, 
+                                "shows": [
+                                {   "show_id": show.id, 
+                                    "time": show.get_time()
+                                }
+                                ]
+                            }
+                        ]
+                    })
+                    flag = True
+        if not flag:
+            data.append({
+                "movie": show.movie.name, 
+                "movie_image": show.movie.small_image, 
+                "per_day": [
+                    {   "date": show.get_date(), 
+                        "day": show.get_day(), 
+                        "screens": [
+                            {   "screen_name": show.screen.name, 
+                                "shows": [
+                                    {
+                                        "show_id": show.id, 
+                                        "time": show.get_time() 
+                                    }
+                                ] 
+                            }
+                        ] 
+                    }
+                ] 
+                })
+    dates = []
+    for show in shows:
+        flag = False
+        for date in dates:
+            if show.get_date() == date['date']:
+                flag = True
+        if not flag:
+            dates.append({"date": show.get_date(), "day": show.get_day()})
+    dates = sorted(dates, key=lambda k: k['date']) 
+    with open("data.json", "w") as file:
+        json.dump(data, file)
+    return render(request, 'theatre.html', {'user': request.user, 'theatre': theatre, 'data': data, 'dates': dates, 'first_date': dates[0]['date']})
 
 def movie(request, movie_name):
     movie = Movie.objects.get(name=movie_name)
@@ -18,7 +89,7 @@ def movie(request, movie_name):
     for show in shows:
         date_flag = False
         for block in data:
-            if show.get_date() in block['date']:
+            if show.get_date() == block['date']:
                 date_flag = True
 
                 time_flag = False
